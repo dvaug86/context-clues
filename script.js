bc50255cad063f265c3b626e6d6b3c274cfa1de8
David Vaughan <dvaug86@gmail.com>
2022-03-29 11:54:55 -0400

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let person = {
-  name: 'John',
-  sayHello: function(){
-    console.log(this.name)
+$(document).ready(function(){
+  
+  var friends = ['Mike', 'JA', 'Collin', 'Cam', 'Matt'];
+  var locations = ['bathroom', 'master bedroom', 'kitchen', 'shower', 'garage', 'pantry', 'closet', 'guest bedroom', 'hall', 'woods'];
+  var weapons = ['pickle','pillow','feather','lightbulb','knife','gun','antlers','mind','dragon','mouse','rope','air','water','smoke','crowbar','hammer','lamp','glass shard','fire','poison','shotgun','magic','pen','paperclip','drill'];
+
+
+
+ //set up the accusations
+  for(var i = 1; i<101; i++){
+    $("body").append((`<h3 id="${i}">Accusation ${i}: </h3>`));
   }
-};
-person.sayHello();
-console.log('end');
+  
+ var friend_accusation = accusations();
+  function accusations(){
+     $('h3').click(function(e) {
+        var f_Index = Math.floor(Math.random() * friends.length); 
+        var l_Index = Math.floor(Math.random() * locations.length);
+        var w_Index = Math.floor(Math.random() * weapons.length);
+        
+        alert(`Accusation ${e.target.id}: I accuses ${friends[f_Index]}, with the ${weapons[w_Index]} in the ${locations[l_Index]}!`);
+   })
+ 
+
+  
+ }
+})
+ 
